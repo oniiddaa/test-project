ebf507aa1090034f55406a319dc42c04a2e74661
ledionlimani <limaniledion501@gmail.com>
2026-06-04 19:17:04 +0200

    ledion

---
+++ ledion/main.js
@@ -0,0 +1,8 @@
+
+console.log("KTM Home Page loaded successfully!");
+
+
+const logo = document.querySelector('.logo');
+logo.addEventListener('click', () => {
+    alert("Welcome to KTM - Ready to Race!");
+});
