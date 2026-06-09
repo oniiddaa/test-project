eedb01e62dd15c57f2b8e96513857d65e4e2f98e
ledionlimani <limaniledion501@gmail.com>
2026-06-09 18:52:25 +0200

    ledion

--- ledion/main.js
+++ ledion/main.js
@@ -6,3 +6,12 @@ const logo = document.querySelector('.logo');
 logo.addEventListener('click', () => {
     alert("Welcome to KTM - Ready to Race!");
 });
+
+const slides = document.querySelectorAll('.slide');
+let current = 0;
+
+setInterval(() => {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+}, 4000);
